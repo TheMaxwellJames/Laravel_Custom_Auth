13f1c6de3f381a7fdc68ce43bb7599c314e3f63c
Maxwell James <maxichanj@gmail.com>
2023-07-19 01:14:47 +0100

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'Login')
+
+
+
+@section('content')
+<h1>Login Page</h1>
+@endsection
+
+
+@section('script')
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+
+</head>
+<body>
+    <main>
+        @yield('content')
+    </main>
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    @yield('script')
+
+</body>
+</html>
